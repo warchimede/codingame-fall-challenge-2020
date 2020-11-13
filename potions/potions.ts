2e113db9b5c2203cdaeb91eebaed94eb904dfc22
William Archimede <william.archimede@protonmail.com>
2020-11-13 22:44:21 +0100

    Try to brew the most expensive

--- potions/potions.ts
+++ potions/potions.ts
@@ -12,14 +12,14 @@ enum ActionType {
 
 class Action {
   identifier: number;
-  type: ActionType;
+  type: string;
   delta0: number;
   delta1: number;
   delta2: number;
   delta3: number;
   price: number;
 
-  constructor(identifier: number, type: ActionType, delta0: number, delta1: number, delta2: number, delta3: number, price: number) {
+  constructor(identifier: number, type: string, delta0: number, delta1: number, delta2: number, delta3: number, price: number) {
       this.identifier = identifier;
       this.type = type;
       this.delta0 = delta0;
@@ -32,35 +32,47 @@ class Action {
 
 // game loop
 while (true) {
+  var actions: Action[] = [];
+  var inventory: number[] = [];
+
   const actionCount: number = parseInt(readline()); // the number of spells and recipes in play
   for (let i = 0; i < actionCount; i++) {
-      var inputs: string[] = readline().split(' ');
-      const actionId: number = parseInt(inputs[0]); // the unique ID of this spell or recipe
-      const actionType: string = inputs[1]; // in the first league: BREW; later: CAST, OPPONENT_CAST, LEARN, BREW
-      const delta0: number = parseInt(inputs[2]); // tier-0 ingredient change
-      const delta1: number = parseInt(inputs[3]); // tier-1 ingredient change
-      const delta2: number = parseInt(inputs[4]); // tier-2 ingredient change
-      const delta3: number = parseInt(inputs[5]); // tier-3 ingredient change
-      const price: number = parseInt(inputs[6]); // the price in rupees if this is a potion
-      const tomeIndex: number = parseInt(inputs[7]); // in the first two leagues: always 0; later: the index in the tome if this is a tome spell, equal to the read-ahead tax; For brews, this is the value of the current urgency bonus
-      const taxCount: number = parseInt(inputs[8]); // in the first two leagues: always 0; later: the amount of taxed tier-0 ingredients you gain from learning this spell; For brews, this is how many times you can still gain an urgency bonus
-      const castable: boolean = inputs[9] !== '0'; // in the first league: always 0; later: 1 if this is a castable player spell
-      const repeatable: boolean = inputs[10] !== '0'; // for the first two leagues: always 0; later: 1 if this is a repeatable player spell
+    var inputs: string[] = readline().split(' ');
+    const actionId: number = parseInt(inputs[0]); // the unique ID of this spell or recipe
+    const actionType: string = inputs[1]; // in the first league: BREW; later: CAST, OPPONENT_CAST, LEARN, BREW
+    const delta0: number = parseInt(inputs[2]); // tier-0 ingredient change
+    const delta1: number = parseInt(inputs[3]); // tier-1 ingredient change
+    const delta2: number = parseInt(inputs[4]); // tier-2 ingredient change
+    const delta3: number = parseInt(inputs[5]); // tier-3 ingredient change
+    const price: number = parseInt(inputs[6]); // the price in rupees if this is a potion
+    const tomeIndex: number = parseInt(inputs[7]); // in the first two leagues: always 0; later: the index in the tome if this is a tome spell, equal to the read-ahead tax; For brews, this is the value of the current urgency bonus
+    const taxCount: number = parseInt(inputs[8]); // in the first two leagues: always 0; later: the amount of taxed tier-0 ingredients you gain from learning this spell; For brews, this is how many times you can still gain an urgency bonus
+    const castable: boolean = inputs[9] !== '0'; // in the first league: always 0; later: 1 if this is a castable player spell
+    const repeatable: boolean = inputs[10] !== '0'; // for the first two leagues: always 0; later: 1 if this is a repeatable player spell
+    
+    const action = new Action(actionId, actionType, delta0, delta1, delta2, delta3, price);
+    actions.push(action);
   }
   for (let i = 0; i < 2; i++) {
-      var inputs: string[] = readline().split(' ');
-      const inv0: number = parseInt(inputs[0]); // tier-0 ingredients in inventory
-      const inv1: number = parseInt(inputs[1]);
-      const inv2: number = parseInt(inputs[2]);
-      const inv3: number = parseInt(inputs[3]);
-      const score: number = parseInt(inputs[4]); // amount of rupees
+    var inputs: string[] = readline().split(' ');
+    const inv0: number = parseInt(inputs[0]); // tier-0 ingredients in inventory
+    const inv1: number = parseInt(inputs[1]);
+    const inv2: number = parseInt(inputs[2]);
+    const inv3: number = parseInt(inputs[3]);
+    const score: number = parseInt(inputs[4]); // amount of rupees
+
+    inventory = [inv0, inv1, inv2, inv3];
   }
 
   // Write an action using console.log()
   // To debug: console.error('Debug messages...');
 
+  // Sort by price
+  actions.sort((a, b) => a.price - b.price);
+  // Brew the most expensive
+  const action = actions[0];
 
   // in the first league: BREW <id> | WAIT; later: BREW <id> | CAST <id> [<times>] | LEARN <id> | REST | WAIT
-  console.log('BREW 0');
+  console.log('${action.type} ${action.identifier}');
 }
 
